b3a140908485faf0ea90f307b16817cece6f5a9a
root <root@localhost.localdomain>
2026-06-07 19:16:27 +0800

    Add Writing nav, fix toKebabCase for Chinese categories

--- src/utils/to-kebab-case.ts
+++ src/utils/to-kebab-case.ts
@@ -2,6 +2,6 @@ const toKebabCase = (str: string = ""): string =>
   str
     .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
     ?.map((word) => word.toLowerCase())
-    .join("-") || "";
+    .join("-") || str;
 
 export { toKebabCase };
